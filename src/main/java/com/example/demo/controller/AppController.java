de82572bfdfedeb184eb652dc8d2fd31acd4c7dc
jonaswallays <fadeinofficial@gmail.com>
2020-02-10 19:00:14 +0100

    Using profiles Basics of ModelAndView

---
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+
+    @Value("${msg}")
+    public String msg;
+
+    @GetMapping("/")
+    public ModelAndView getIndexPage(){
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", msg);
+        return mv;
+    }
+}
