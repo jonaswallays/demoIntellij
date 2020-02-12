78ef375cbf3caf5d5648b1823e6dece364577487
jonaswallays <fadeinofficial@gmail.com>
2020-02-12 19:53:00 +0100

    setup init data

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,5 @@
+/*delete from book;
+insert into book(book_title, isbin, authors, publish_date, publisher, price, stock)
+         values ("Book 1", "9783161484100", "auteur1", "2018-10-14", "Phantom", 45.20, 55);
+insert into book(book_title, isbin, authors, publish_date, publisher, price, stock)
+         values ("Book 2", "9783161484101", "auteur2", "2018-10-14", "Phantom", 35.20, 50);*/
